0975470d32a7a28c388fef543751ab6b8f1ea344
Tom <tomislav_lokas@dell.com>
2020-03-23 14:25:56 +0000

    the dockers

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace aspnetcoreapp
+{
+    public class Startup
+    {
+        public bool Stuff() 
+        {
+            return false;
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("TOMS Success! The Framework Training template microservice is up and running using ASP.NET Core!");
+            });
+        }
+    }
+}
